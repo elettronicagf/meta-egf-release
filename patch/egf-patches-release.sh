ea83ec5584d176dcdb5bab1586100e1c0e7e595c
yocto <yocto@dockerbuilder>
2023-02-22 10:53:39 +0100

    Aggiunte cartelle di patch e di configurazione

---
+++ patch/egf-patches-release.sh
@@ -0,0 +1,5 @@
+# This map lists patch source and destination : "patch_src patch_dest" 
+# - patch_src -> path of the patch starting from ./meta-egf/scripts/egf-conf/egf-release
+# - patch_dest-> path of the destination from  source (../meta-egf)
+
+PATCH_MAP_RELEASE=( )
